91bef0e064fbfbc91727865530a30f80acaede95
zohra-neda <zohra.neda@gmail.com>
2024-04-28 17:47:24 +0430

    Update header text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <h2 className={styles.sectionHeadText}>Skills.</h2>
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
