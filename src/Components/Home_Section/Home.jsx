ae542758f259171ebeeec4a0c47ddb6d626fd277
zohra-neda <zohra.neda@gmail.com>
2024-09-04 16:12:14 +0430

    Add initial Home component with basic layout, intro text, and contact section

---
+++ src/Components/Home_Section/Home.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import './home.scss'
+import { AiFillGithub } from 'react-icons/ai'
+import { FiTwitter, FiYoutube } from 'react-icons/fi'
+import { TbArrowBigRightLines, TbChevronsDown } from 'react-icons/tb'
+
+const Home = () => {
+  return (
+    <section id='home' className="home section">
+      {/* this div will be only visible at wider screen */}
+      {/* <div className="leftIcons">
+        <div className="socials grid">
+          <a href='' target="_blank">
+            <AiFillGithub className="icon"/>
+          </a>
+          <a href='' target="_blank">
+            <FiTwitter className="icon"/>
+          </a>
+          <a href='' target="_blank">
+            <FiYoutube className="icon"/>
+          </a>
+          <div className="line"></div>
+        </div>
+      </div> */}
+
+      {/* homeContent */}
+      <div className="container homeContainer">
+        <span className="introText">
+          Hi my name is,
+        </span>
+        <h1 className="title">
+          Zohra Neda
+        </h1>
+        <span className="subTitle">
+          I develope scalable web based applications and software.
+        </span>
+
+        <p className="homeParagraph">
+          I code within required coding standards and styles to serve the exact need.
+        </p>
+
+        <div className="lowerHomeSection">
+          <button className="contactBtn">
+            <a href="#contact" className="flex">
+              Contact Me <TbArrowBigRightLines className="icon" />
+            </a>
+          </button>
+
+          <div className="scrollDiv">
+            <a href="#about" className="flex">
+              <h6 className="scroll">Scroll Down</h6> <TbChevronsDown className="icon"/>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* right div, with email address ( will also be visible at a wider screen only) */}
+      {/* <div className="rightEmail">
+        <div>
+          <div className="emailAddress">
+            <a href="https://mailto:youremail@gmail.com" target="_blank">
+              zohra.neda@gmail.com
+            </a>
+          </div>
+          <div className="line"></div>
+        </div>
+      </div> */}
+    </section>
+  )
+}
+
+export default Home
